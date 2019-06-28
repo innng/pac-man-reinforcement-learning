d1e9cde4d61dc1e365dd241bd7b22a46729b8d8d
Ingrid Rosselis <irscunha@dcc.ufmg.br>
2019-06-28 10:16:15 -0300

    Add auxiliary functions to read input file and create a q_table

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+
 """Implement the Q-Learning algorithm for Pac-Man game."""
 
+import numpy as np
 from sys import argv
 from decimal import Decimal
 
@@ -7,6 +9,7 @@ states = ["-", "0", "&"]
 actions = ["U", "D", "L", "R"]
 
 final_states = ["0", "&"]
+wall = "#"
 
 rewards = {"-": -1, "0": 10, "&": -10}
 movement = {
@@ -32,5 +35,31 @@ episodes = int(argv[4])
 value_file = "q.txt"
 policy_file = "pi.txt"
 
+
+def read_input_file():
+    """Get the number of rows and cols and the maze from input file."""
+    global rows
+    global cols
+    global maze
+
+    with open(filename, "r") as fout:
+        a = fout.readline()
+        rows, cols = tuple(a.split())
+        rows = int(rows)
+        cols = int(cols)
+
+        maze = fout.readlines()
+        maze = [list(r.strip(" \n")) for r in maze]
+        maze = np.matrix(maze, dtype=str)
+
+
+def create_q_table():
+    """Create a table for states x actions values."""
+    global q_table
+
+    q_table = np.zeros((len(states), len(actions)), dtype=Decimal)
+
+
 if __name__ == "__main__":
-    pass
+    read_input_file()
+    create_q_table()
